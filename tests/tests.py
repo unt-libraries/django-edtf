9e6c13791464647858546b24daf05205416b2750
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-08-19 12:19:56 -0500

    Removed empty tests and simplified some tests.

--- tests/tests.py
+++ tests/tests.py
@@ -23,7 +23,7 @@ class TestEdtfFormView(SimpleTestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
-    def test_view_returns_200(self):
+    def test_returns_200(self):
         request = self.factory.get('/')
         response = edtf_form(request)
         self.assertEqual(response.status_code, 200)
@@ -32,12 +32,6 @@ class TestEdtfFormView(SimpleTestCase):
         url = reverse('edtf:edtf_form')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'edtf/edtf_form.html')
-
-    def test_validates_compliant_extended_date_time(self):
-        pass
-
-    def test_does_not_validate_non_compliant_extended_date_time(self):
-        pass
 
 
 class TestBooleanResultView(SimpleTestCase):
@@ -51,30 +45,24 @@ class TestBooleanResultView(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_returns_400_without_date(self):
-        level = '1'
-        request = self.factory.get('/', {'level': level})
+        request = self.factory.get('/', {'level': '1'})
         response = boolean_result(request)
         self.assertEqual(response.status_code, 400)
 
     # The view should return an HTTP 400 response when no level is present.
     @expectedFailure
     def test_returns_400_without_level(self):
-        date = 'March 3rd, 2016'
-        request = self.factory.get('/', {'date': date})
+        request = self.factory.get('/', {'date': 'March 3rd, 2016'})
         response = boolean_result(request)
         self.assertEqual(response.status_code, 400)
 
     def test_returns_true_with_valid_date(self):
-        date = '2012-06-12'
-        level = '0'
-        request = self.factory.get('/', {'date': date, 'level': level})
+        request = self.factory.get('/', {'date': '2012-06-12', 'level': '0'})
         response = boolean_result(request)
         self.assertHTMLEqual(response.content, 'true')
 
     def test_returns_false_with_invalid_date(self):
-        date = 'March 3rd, 2016'
-        level = '2'
-        request = self.factory.get('/', {'date': date, 'level': level})
+        request = self.factory.get('/', {'date': 'March 3rd, 2016', 'level': '2'})
         response = boolean_result(request)
         self.assertHTMLEqual(response.content, 'false')
 
@@ -89,25 +77,21 @@ class TestResultJsonView(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_returns_true_with_valid_date(self):
-        date = '2013?-12-12'
-        response = self.client.get(reverse('edtf:results_json'), {'date': date})
+        response = self.client.get(reverse('edtf:results_json'), {'date': '2013?-12-12'})
         self.assertEqual(json.loads(response.content), {'validEDTF': True})
 
     def test_returns_false_with_invalid_date(self):
-        date = '2013-23-????'
-        response = self.client.get(reverse('edtf:results_json'), {'date': date})
+        response = self.client.get(reverse('edtf:results_json'), {'date': '2013-23-????'})
         self.assertEqual(json.loads(response.content), {'validEDTF': False})
 
     def test_returns_400_without_date(self):
-        level = '1'
-        request = self.factory.get('/', {'level': level})
+        request = self.factory.get('/', {'level': '1'})
         response = result_json(request)
         self.assertEqual(response.status_code, 400)
 
     # The view should return an HTTP 400 response when no level is present.
     @expectedFailure
     def test_returns_400_without_level(self):
-        date = 'March 3rd, 2016'
-        request = self.factory.get('/', {'date': date, 'level': ''})
+        request = self.factory.get('/', {'date': 'March 3rd, 2016', 'level': ''})
         response = result_json(request)
         self.assertEqual(response.status_code, 400)
