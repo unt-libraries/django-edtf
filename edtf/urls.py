0f4aa81ee3bec9edb1d0f6219e2e34cabd8f9d81
madhulika95b <srisudhamadhulika.bayyavarapu@unt.edu>
2021-01-20 15:10:29 -0600

    Upgrade code to comply with Django 2.2

--- edtf/urls.py
+++ edtf/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from edtf.views import edtf_form, result_json
 
 app_name = 'edtf'
 urlpatterns = [
-    url(r'^$', edtf_form, name='edtf_form'),
-    url(r'^isValid.json$', result_json, name='results_json'),
+    path('', edtf_form, name='edtf_form'),
+    path('isValid.json', result_json, name='results_json'),
 ]
